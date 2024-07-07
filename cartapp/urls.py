01145b2675a91999fb09cea6e6d14d77a0c9e12d
nihala shirin <nihalashirin02@gmail.com>
2024-07-08 02:13:34 +0530

    i have complete cart and checkout

--- cartapp/urls.py
+++ cartapp/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
     path('cart_view/', views.cart_view, name='cart_view'),
     path('quantity_upadate/', views.quantity_upadate, name='quantity_upadate'),
+    path('proceed_to_checkout/', views.proceed_to_checkout, name='proceed_to_checkout'),
+    path('place_order/', views.place_order, name='place_order'),
+    
     
     # path('proceed/',views.proceed,name='proceed'),
     
--- cartapp/views.py
+++ cartapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect,get_object_or_404
 from products.models import newproducts
 from logintohome.models import CustomUser
 from  cartapp.models import MyCart
+from django.http import JsonResponse
+from userprofile.models import UserAddress
 # Create your views here.
 
 
@@ -41,7 +43,7 @@ def cart_view(request):
     user_id = CustomUser.objects.get(email=user)
     print(vars(user_id), 'User Details')
     
-    cart_items = MyCart.objects.filter(user_id=user_id.id)
+    cart_items = MyCart.objects.filter(user_id=user_id.id).order_by('product_id')
     
     sub_total = 0
     for item in cart_items:
@@ -57,15 +59,34 @@ def proceed(request):
     return render(request,'userside/checkout.html')
 
 def quantity_upadate(request):
-    print('loooooooooooooooooooooooooooo')
+    user_email=request.session['email']
+    user_id=CustomUser.objects.get(email=user_email)
     if request.method=='POST':
         print(request.POST,"saf")
+        prod_id=request.POST.get('product_id') 
+        count = int(request.POST.get('count'))
+        prduct_size=request.POST.get('size')
+    cart_product=MyCart.objects.get(user_id=user_id.id,product_id=prod_id,size=prduct_size)
+    cart_product.quantity+=count 
+    cart_product.save()
+    return JsonResponse({'status':'success'})
 
-#         email=request.session['email']
-#         customer=CustomUser.objects.get(email=email)
-        
-#         cart=MyCart.objects.get(user_id=customer.id)
-#         cart.quantity=new_quantity
-#         cart.save()
-#         return render(requestZZZZZZZZZZZZZZZZZZZZZZZZZZZZZz)
-            
+
+def proceed_to_checkout(request):
+    user_email = request.session.get('email')
+    user = CustomUser.objects.get(email=user_email)
+    cart_items = MyCart.objects.filter(user_id=user.id)
+    addresses=UserAddress.objects.filter(user_id=user.id)
+    sub_total = 0
+    for item in cart_items:
+        sub_total += item.product.price * item.quantity
+    
+    context = {
+        'cart_items': cart_items,
+        'sub_total': sub_total,
+        'addresses':addresses
+    }
+    return render(request, 'userside/checkout.html', context)
+
+def place_order(request):
+    print(request.POST,"i love you da")
